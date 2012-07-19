2bb7114349c4599633095795f8bd250038c1d8a4
hugoyq <hugo@yoquie.ro>
2012-07-18 22:47:25 -0400

    primera subida

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML >
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link type="text/css" rel="stylesheet" href="css/style.css" />
+        <script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+   
+        <title>PRACTICA DE HUGO</title>
+    </head>
+    <body>
+      <header class="barra">
+  hola
+  <nav id="nave">
+
+ <div class="jaja"></div>
+ <div class="jaja"></div>
+ <div class="jaja"></div>
+
+  </nav>
+      </header>
+    </body>
+</html>
